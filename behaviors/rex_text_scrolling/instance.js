f64c59beed9598d456746110dd9d3773d2d92553
Hua <binhua.tein@gmail.com>
2017-06-30 18:08:01 +0800

    Update

---
+++ behaviors/rex_text_scrolling/instance.js
@@ -0,0 +1,26 @@
+"use strict";
+
+{
+	const BEHAVIOR_CLASS = SDK.Behaviors.Rex_text_scrolling;
+
+	BEHAVIOR_CLASS.Instance = class Rex_text_scrollingInstance extends SDK.IBehaviorInstanceBase
+	{
+		constructor(sdkBehType, behInst)
+		{
+			super(sdkBehType, behInst);
+		}
+		Release()
+		{
+		}
+		OnCreate()
+		{
+		}
+		OnPropertyChanged(id, value)
+		{
+		}
+		LoadC2Property(name, valueString)
+		{
+			return false;       // not handled
+		}
+	};
+}
---
+++ behaviors/rex_text_scrolling/type.js
@@ -0,0 +1,13 @@
+"use strict";
+
+{
+	const BEHAVIOR_CLASS = SDK.Behaviors.Rex_text_scrolling;
+
+	BEHAVIOR_CLASS.Type = class Rex_text_scrollingType extends SDK.IBehaviorTypeBase
+	{
+		constructor(sdkPlugin, iBehaviorType)
+		{
+			super(sdkPlugin, iBehaviorType);
+		}
+	};
+}
