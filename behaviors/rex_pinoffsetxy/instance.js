8b2015e6916da4411c448bff6c14ef94b7ae8e43
Hua <binhua.tein@gmail.com>
2017-08-12 17:04:48 +0800

    Update

---
+++ behaviors/rex_pinoffsetxy/instance.js
@@ -0,0 +1,26 @@
+"use strict";
+
+{
+	const BEHAVIOR_CLASS = SDK.Behaviors.Rex_pinOffsetXY;
+
+	BEHAVIOR_CLASS.Instance = class Rex_pinOffsetXYInstance extends SDK.IBehaviorInstanceBase
+	{
+		constructor(sdkBehType, behInst)
+		{
+			super(sdkBehType, behInst);
+		}
+		Release()
+		{
+		}
+		OnCreate()
+		{
+		}
+		OnPropertyChanged(id, value)
+		{
+		}
+		LoadC2Property(name, valueString)
+		{
+			return false;       // not handled
+		}
+	};
+}
---
+++ behaviors/rex_pinoffsetxy/type.js
@@ -0,0 +1,13 @@
+"use strict";
+
+{
+	const BEHAVIOR_CLASS = SDK.Behaviors.Rex_pinOffsetXY;
+
+	BEHAVIOR_CLASS.Type = class Rex_pinOffsetXYType extends SDK.IBehaviorTypeBase
+	{
+		constructor(sdkPlugin, iBehaviorType)
+		{
+			super(sdkPlugin, iBehaviorType);
+		}
+	};
+}
