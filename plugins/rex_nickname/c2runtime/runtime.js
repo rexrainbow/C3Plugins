14df5717e12befa3291c78a1ae9890e73ca26fa4
Hua <binhua.tein@gmail.com>
2017-08-13 19:47:35 +0800

    Update

--- plugins/rex_nickname/c2runtime/runtime.js
+++ plugins/rex_nickname/c2runtime/runtime.js
@@ -88,7 +88,7 @@ cr.plugins_.Rex_Nickname.AddNickname = function(nickname, objtype)
         if (objtype == null)
             return null;
         
-        var inst = window.RexC2CreateObject.call(this, objtype, layer, x, y, callback, ignore_picking);
+        var inst = window.rexObjs.CreateObject.call(this, objtype, layer, x, y, callback, ignore_picking);
         return inst;           
 	}; 	
 
@@ -303,75 +303,3 @@ cr.plugins_.Rex_Nickname.AddNickname = function(nickname, objtype)
 	
 	
 }());
-
-(function ()
-{
-    // general CreateObject function which call a callback before "OnCreated" triggered
-    if (window.RexC2CreateObject != null)
-        return;
-        
-    // copy from system action: CreateObject
-    var CreateObject = function (obj, layer, x, y, callback, ignore_picking)
-    {
-        if (!layer || !obj)
-            return;
-
-        var inst = this.runtime.createInstance(obj, layer, x, y);
-		
-		if (!inst)
-			return;
-		
-		this.runtime.isInOnDestroy++;
-		
-		// call callback before "OnCreated" triggered
-		if (callback)
-		    callback(inst);
-		// call callback before "OnCreated" triggered
-		
-		var i, len, s;
-		this.runtime.trigger(Object.getPrototypeOf(obj.plugin).cnds.OnCreated, inst);
-		
-		if (inst.is_contained)
-		{
-			for (i = 0, len = inst.siblings.length; i < len; i++)
-			{
-				s = inst.siblings[i];
-				this.runtime.trigger(Object.getPrototypeOf(s.type.plugin).cnds.OnCreated, s);
-			}
-		}
-		
-		this.runtime.isInOnDestroy--;
-
-        if (ignore_picking !== true)
-        {
-            // Pick just this instance
-            var sol = obj.getCurrentSol();
-            sol.select_all = false;
-		    sol.instances.length = 1;
-		    sol.instances[0] = inst;
-		
-		    // Siblings aren't in instance lists yet, pick them manually
-		    if (inst.is_contained)
-		    {
-			    for (i = 0, len = inst.siblings.length; i < len; i++)
-			    {
-				    s = inst.siblings[i];
-				    sol = s.type.getCurrentSol();
-				    sol.select_all = false;
-				    sol.instances.length = 1;
-				    sol.instances[0] = s;
-			    }
-		    }
-        }
-
-        // add solModifiers
-        //var current_event = this.runtime.getCurrentEventStack().current_event;
-        //current_event.addSolModifier(obj);
-        // add solModifiers
-        
-		return inst;
-    };
-    
-    window.RexC2CreateObject = CreateObject;
-}());
-    
