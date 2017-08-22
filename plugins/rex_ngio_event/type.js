66f03bcd92df878699294572bdebc5266e39adcd
Hua <binhua.tein@gmail.com>
2017-08-22 14:12:50 +0800

    Update

---
+++ plugins/rex_ngio_event/type.js
@@ -0,0 +1,13 @@
+"use strict";
+
+{
+	const PLUGIN_CLASS = SDK.Plugins.Rex_NGIO_Event;
+
+	PLUGIN_CLASS.Type = class Rex_NGIO_EventType extends SDK.ITypeBase
+	{
+		constructor(sdkPlugin, iObjectType)
+		{
+			super(sdkPlugin, iObjectType);
+		}
+	};
+}
