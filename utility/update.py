6299d131f06f6209d102ba94bb493cec6f331d92
Hua <binhua.tein@gmail.com>
2017-09-01 18:24:13 +0800

    Update

---
+++ utility/update.py
@@ -0,0 +1,33 @@
+import sys, os
+import os.path as op
+from shutil import copyfile
+
+rootDir = r"D:\Construct 3\my_data\C3Plugins"
+srcDir = r"D:\Construct 3\my_data\C3Plugins\utility"
+def getPath(targetFileName):
+    p = []
+    for (dirpath, dirnames, filenames) in os.walk(rootDir):
+        if dirpath == srcDir:
+            continue
+
+        if targetFileName in filenames:
+            p.append(op.join(dirpath, targetFileName))
+
+    return p
+
+def main():
+    for arg in sys.argv[1:]:
+        pathes = getPath( os.path.split(arg)[1] )
+        for p in pathes:
+            print "copy {src} to {dist}".format(src=arg, dist = p)
+            copyfile(arg, p)
+
+
+
+# -----------------------------------------------------------------------------  
+try:
+    main()
+except Exception, e: 
+    print e
+
+raw_input()
