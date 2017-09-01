e6e3d8972f14c17e89255e0c3c21b4217c042e26
Hua <binhua.tein@gmail.com>
2017-09-01 18:23:51 +0800

    Update

--- behaviors/rex_fsm/c2runtime/rex_fsm.js
+++ behaviors/rex_fsm/c2runtime/rex_fsm.js
@@ -1,73 +1,67 @@
-(function ()
-{
+(function () {
     if (!window.rexObjs)
         window.rexObjs = {};
-    
+
     if (window.rexObjs.FSMKlass)
         return;
 
-    window.rexObjs.FSMKlass = function()
-    {
+    window.rexObjs.FSMKlass = function () {
         this.OnGetNextState = null;
         this.OnTransfer = null;
-        this.OnExit = null;      
+        this.OnExit = null;
         this.OnEnter = null;
-        this.OnStateChanged = null;  
+        this.OnStateChanged = null;
     };
     var FSMKlassProto = window.rexObjs.FSMKlass.prototype;
 
-    FSMKlassProto.Init = function (previousState, currentState)
-    {
+    FSMKlassProto.Init = function (previousState, currentState) {
         this.PreState = previousState;
         this.CurState = currentState;
-    }; 
-    	
-    FSMKlassProto.Request = function(newState)
-    {
-        if (newState == null)
-        {
+    };
+
+    FSMKlassProto.Request = function (newState) {
+        if (newState == null) {
             if (this.OnGetNextState)
-                newState = this.OnGetNextState();            
+                newState = this.OnGetNextState();
             if (newState == null)
                 return;
         }
-            
+
         // newState != null: state transfer
         this.PreState = this.CurState;
         this.CurState = newState;
-                
+
         var preState = this.PreState;
         var curState = this.CurState;
-        
+
         // trigger OnStateChanged first
         if (this.OnStateChanged)
             this.OnStateChanged();
-                        
+
         // try to run transfer_action
         var hasCalled;
         if (this.OnTransfer)
-            hasCalled = this.OnTransfer(preState, curState);             
+            hasCalled = this.OnTransfer(preState, curState);
         if (hasCalled)
             return;
-         
+
         // no transfer_action found
         if (this.OnExit)
             this.OnExit(preState);
 
         if (this.OnEnter)
             this.OnEnter(curState);
-    };  
+    };
 
-	FSMKlassProto.saveToJSON = function ()
-	{    
-		return { "ps": this.PreState,
-		         "cs": this.CurState
-			   };
-	};
-	
-	FSMKlassProto.loadFromJSON = function (o)
-	{
-	    this.PreState = o["ps"];
-		this.CurState = o["cs"];
-	};	
+    FSMKlassProto.saveToJSON = function () {
+        return {
+            "ps": this.PreState,
+            "cs": this.CurState
+        };
+    };
+
+    FSMKlassProto.loadFromJSON = function (o) {
+        this.PreState = o["ps"];
+        this.CurState = o["cs"];
+    };
 }());
--- plugins/rex_nickname/c2runtime/rex_createObject.js
+++ plugins/rex_nickname/c2runtime/rex_createObject.js
@@ -1,72 +1,65 @@
 
-(function ()
-{
-    if (!window.rexObjs)
-        window.rexObjs = {};
-    
-    // general CreateObject function which call a callback before "OnCreated" triggered
-    if (window.rexObjs.CreateObject)
-        return;
+(function () {
+	if (!window.rexObjs)
+		window.rexObjs = {};
 
-    // copy from system action: CreateObject
-    window.rexObjs.CreateObject = function (obj, layer, x, y, callback, ignore_picking)
-    {
-        if (!layer || !obj)
-            return;
+	// general CreateObject function which call a callback before "OnCreated" triggered
+	if (window.rexObjs.CreateObject)
+		return;
 
-        var inst = this.runtime.createInstance(obj, layer, x, y);
-		
+	// copy from system action: CreateObject
+	window.rexObjs.CreateObject = function (obj, layer, x, y, callback, ignore_picking) {
+		if (!layer || !obj)
+			return;
+
+		var inst = this.runtime.createInstance(obj, layer, x, y);
+
 		if (!inst)
 			return;
-		
+
 		this.runtime.isInOnDestroy++;
-		
+
 		// call callback before "OnCreated" triggered
 		if (callback)
-		    callback(inst);
+			callback(inst);
 		// call callback before "OnCreated" triggered
-		
+
 		var i, len, s;
 		this.runtime.trigger(Object.getPrototypeOf(obj.plugin).cnds.OnCreated, inst);
-		
-		if (inst.is_contained)
-		{
-			for (i = 0, len = inst.siblings.length; i < len; i++)
-			{
+
+		if (inst.is_contained) {
+			for (i = 0, len = inst.siblings.length; i < len; i++) {
 				s = inst.siblings[i];
 				this.runtime.trigger(Object.getPrototypeOf(s.type.plugin).cnds.OnCreated, s);
 			}
 		}
-		
+
 		this.runtime.isInOnDestroy--;
 
-        if (ignore_picking !== true)
-        {
-            // Pick just this instance
-            var sol = obj.getCurrentSol();
-            sol.select_all = false;
-		    sol.instances.length = 1;
-		    sol.instances[0] = inst;
-		
-		    // Siblings aren't in instance lists yet, pick them manually
-		    if (inst.is_contained)
-		    {
-			    for (i = 0, len = inst.siblings.length; i < len; i++)
-			    {
-				    s = inst.siblings[i];
-				    sol = s.type.getCurrentSol();
-				    sol.select_all = false;
-				    sol.instances.length = 1;
-				    sol.instances[0] = s;
-			    }
-		    }
-        }
+		if (ignore_picking !== true) {
+			// Pick just this instance
+			var sol = obj.getCurrentSol();
+			sol.select_all = false;
+			sol.instances.length = 1;
+			sol.instances[0] = inst;
 
-        // add solModifiers
-        //var current_event = this.runtime.getCurrentEventStack().current_event;
-        //current_event.addSolModifier(obj);
-        // add solModifiers
-        
+			// Siblings aren't in instance lists yet, pick them manually
+			if (inst.is_contained) {
+				for (i = 0, len = inst.siblings.length; i < len; i++) {
+					s = inst.siblings[i];
+					sol = s.type.getCurrentSol();
+					sol.select_all = false;
+					sol.instances.length = 1;
+					sol.instances[0] = s;
+				}
+			}
+		}
+
+		// add solModifiers
+		//var current_event = this.runtime.getCurrentEventStack().current_event;
+		//current_event.addSolModifier(obj);
+		// add solModifiers
+
 		return inst;
-    };
+	};
 }());
