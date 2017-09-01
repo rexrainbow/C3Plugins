c40d4de32adf785c60a270c25dd8cf60fe8d251f
Hua <binhua.tein@gmail.com>
2017-09-01 18:59:11 +0800

    Update

---
+++ utility/index.md
@@ -0,0 +1,28 @@
+# Reference  
+- rex_boardLXYZCache.js  
+  - plugins: rex_board  
+- rex_createObject.js  
+  - plugins: rex_board  
+  - plugins: rex_nickname  
+- rex_csvToArray.js  
+  - plugins: rex_csv  
+  - plugins: rex_csv2array  
+- rex_firebaseAddAfterInitializeHandler.js  
+  - plugins: rex_firebase_apiV3  
+- rex_firebaseCallbackMapKlass.js  
+  - plugins: rex_firebase_apiV3  
+- rex_firebaseItemListKlass.js  
+  - plugins: rex_firebase_apiV3  
+- rex_fsm.js  
+  - behaviors: rex_fsm  
+  - plugins: rex_gfsm  
+- rex_keys2Value.js  
+  - behaviors: rex_bHash  
+  - plugins: rex_firebase_apiV3  
+  - plugins: rex_hash  
+  - plugins: rex_jsshell  
+- rex_pickUIDs.js  
+  - plugins: rex_board  
+- rex_shuffleArr.js  
+  - behaviors: rex_bHash  
+  - plugins: rex_hash  
---
+++ utility/indexGen.py
@@ -0,0 +1,42 @@
+import os
+import os.path as op
+
+rootDir = r"D:\Construct 3\my_data\C3Plugins"
+srcDir = r"D:\Construct 3\my_data\C3Plugins\utility"
+def getPaths(targetFileName):
+    p = []
+    for (dirpath, dirnames, filenames) in os.walk(rootDir):
+        if dirpath == srcDir:
+            continue
+
+        if targetFileName in filenames:
+            p.append(op.join(dirpath, targetFileName))
+
+    return p
+
+
+def output(f2plugins):
+    myPath = op.dirname(op.realpath(__file__))
+    mdF = open(op.join(myPath, "index.md"), "w")    
+    mdF.write("# Reference  \n")
+    for (src, plugins) in f2plugins:
+        mdF.write("- {src}  \n".format(src=src))
+        for p in plugins:
+            p = p.replace(rootDir, "").split("\\")           
+            mdF.write("  - {plugin}: {name}  \n".format(plugin=p[1], name=p[2]))
+
+    mdF.close()
+
+def main():
+    f2plugins = []
+    for (dirpath, dirnames, filenames) in os.walk(srcDir):
+        for f in filenames:
+            if op.splitext(f)[1] != ".js":
+                continue
+
+            paths = getPaths( f )
+            f2plugins.append([f, paths])
+
+    output(f2plugins)
+
+main()
